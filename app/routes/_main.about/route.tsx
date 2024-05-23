03dc6184a6cd1d3c589d54de2a8209c59bc7db8f
Tifany <tifsnunes@gmail.com>
2024-05-23 18:27:40 -0300

    style: animations in about page

--- app/routes/_main.about/route.tsx
+++ app/routes/_main.about/route.tsx
@@ -1,4 +1,5 @@
 import { AboutDetails } from "./AboutDetails"
+import { motion } from "framer-motion"
 
 const educations = [
 	{
@@ -51,69 +52,80 @@ const certificates = [
 
 export default function About() {
 	return (
-		<div className="flex flex-col items-centerjustify-center">
-			<div>
-				<div className="">
-					<div className="w-full  overflow-y-auto">
-						<h1 className="mt-20 text-3xl font-bold tracking-tight sm:text-4xl">
-							Sobre
-						</h1>
-						<p className="max-w-screen-lg tracking-tight py-10 gap-x-8 gap-y-20 px-6 lg:px-8 ">
-							Sou desenvolvedora Full Stack, sempre busco aprimorar minhas
-							habilidades tanto em front-end quanto ao Back-end. Meu nome é
-							Tifany, tenho 23 anos, sou formada em{" "}
-							<span className="font-semibold">
-								Analise e Desenvolvimento de Sistemas
-							</span>{" "}
-							pelo Centro Universitário Vale do Salgado. Onde adquiri uma base
-							sólida em algoritmos, estrutura de dados e programação orientada a
-							objetos. Além disso, estou sempre em busca de aprendizado
-							contínuo, seja através de cursos online, workshops ou projetos
-							pessoais. Estou comprometida em criar soluções eficientes e
-							elegantes para os desafios tecnológicos que encontro. Tenho uma
-							sólida experiência em desenvolvimento web, com mais de dois anos
-							trabalhando com projetos.
-							<br />
-							Como{" "}
-							<span className="font-semibold">desenvolvedora Fullstack</span>,
-							estou sempre em busca de novos desafios e oportunidades para
-							expandir meu conhecimento e contribuir para projetos inovadores.
-							Estou comprometida em entregar soluções de alta qualidade,
-							alinhadas com as melhores práticas da indústria e as necessidades
-							do cliente.
-						</p>
-					</div>
+		<div className="flex flex-col items-centerjustify-center overflow-hidden">
+			<div className="">
+				<div className="w-full ">
+					<motion.h1
+						initial={{ x: "100%" }}
+						animate={{ x: 0 }}
+						transition={{ duration: 1, ease: "easeOut" }}
+						className="mt-20 text-3xl font-bold tracking-tight sm:text-4xl">
+						Sobre
+					</motion.h1>
+					<motion.p
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 1.5 }}
+						className="max-w-screen-lg tracking-tight py-10 gap-x-8 gap-y-20 px-6 lg:px-8 ">
+						Sou desenvolvedora Full Stack, sempre busco aprimorar minhas
+						habilidades tanto em front-end quanto ao Back-end. Meu nome é
+						Tifany, tenho 23 anos, sou formada em{" "}
+						<span className="font-semibold">
+							Analise e Desenvolvimento de Sistemas
+						</span>{" "}
+						pelo Centro Universitário Vale do Salgado. Onde adquiri uma base
+						sólida em algoritmos, estrutura de dados e programação orientada a
+						objetos. Além disso, estou sempre em busca de aprendizado contínuo,
+						seja através de cursos online, workshops ou projetos pessoais. Estou
+						comprometida em criar soluções eficientes e elegantes para os
+						desafios tecnológicos que encontro. Tenho uma sólida experiência em
+						desenvolvimento web, com mais de dois anos trabalhando com projetos.
+						<br />
+						Como <span className="font-semibold">desenvolvedora Fullstack</span>
+						, estou sempre em busca de novos desafios e oportunidades para
+						expandir meu conhecimento e contribuir para projetos inovadores.
+						Estou comprometida em entregar soluções de alta qualidade, alinhadas
+						com as melhores práticas da indústria e as necessidades do cliente.
+					</motion.p>
 				</div>
+			</div>
 
-				<div className="mt-20">
-					<h1 className="text-3xl font-bold tracking-tight sm:text-4xl">
-						Carreira
-					</h1>
-				</div>
+			<div className="mt-20">
+				<motion.h1
+					initial={{ x: "100%" }}
+					animate={{ x: 0 }}
+					transition={{ duration: 1, ease: "easeOut" }}
+					className="text-3xl font-bold tracking-tight sm:text-4xl">
+					Carreira
+				</motion.h1>
+			</div>
 
-				{/* <div className="py-10 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+			{/* <div className="py-10 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
 					<h2 className="text-2xl font-bold tracking-tight">
 						Linguagens e Tecnologias
 					</h2>
 				</div> */}
 
-				<div className="py-10">
-					<div className="mx-auto grid max-w-7xl gap-x-60 px-6 lg:px-8 xl:grid-cols-2">
-						<div className="max-w-2x">
-							<h2 className="text-2xl font-bold tracking-tight">Educação</h2>
-							<AboutDetails items={educations} />
-						</div>
-
-						<div className="max-w-2xl">
-							<h2 className="text-2xl font-bold tracking-tight">Experiência</h2>
-							<AboutDetails items={exp} />
-						</div>
+			<div className="py-10">
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: 1.5 }}
+					className="mx-auto grid max-w-7xl gap-x-60 px-6 lg:px-8 xl:grid-cols-2">
+					<div className="max-w-2x">
+						<h2 className="text-2xl font-bold tracking-tight">Educação</h2>
+						<AboutDetails items={educations} />
 					</div>
 
-					<div className="max-w-2xl px-6">
-						<h2 className="text-2xl font-bold tracking-tight">Certificados</h2>
-						<AboutDetails items={certificates} />
+					<div className="max-w-2xl">
+						<h2 className="text-2xl font-bold tracking-tight">Experiência</h2>
+						<AboutDetails items={exp} />
 					</div>
+				</motion.div>
+
+				<div className="max-w-2xl px-6">
+					<h2 className="text-2xl font-bold tracking-tight">Certificados</h2>
+					<AboutDetails items={certificates} />
 				</div>
 			</div>
 		</div>
