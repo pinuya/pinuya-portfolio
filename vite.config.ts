1c984b30f79b175181391b694ad44b3bca84018e
Tifany <tifsnunes@gmail.com>
2024-05-24 14:33:17 -0300

    add: vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,16 @@ import { installGlobals } from "@remix-run/node"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import svgr from "vite-plugin-svgr"
+import { vercelPreset } from "@vercel/remix/vite"
 
 installGlobals()
 
 export default defineConfig({
-	plugins: [remix(), tsconfigPaths(), svgr()],
+	plugins: [
+		remix({
+			presets: [vercelPreset()],
+		}),
+		tsconfigPaths(),
+		svgr(),
+	],
 })
