afcfbc140d9c959a8c2c346c274535a1a128adbe
Tifany Alves <tifsnunes@gmail.com>
2024-05-26 16:11:12 -0300

    Projects page update (#2)
    * refactor: project page styles and props
    
    * refactor: flex to grid columns and project route animation

--- app/routes/_main.projects/ProjectsDetails.tsx
+++ app/routes/_main.projects/ProjectsDetails.tsx
@@ -1,59 +1,49 @@
 import { Link } from "@remix-run/react"
 import { motion } from "framer-motion"
+import { ScrollArea, ScrollBar } from "~/components/ui/scroll-area"
 
-type Items = {
+type Projects = {
 	title: string
 	desc: string
 	bannerImg: string
-	socialIcons?: string[]
+	linkRep: string
 }
 
 type Props = {
-	items: Items[]
+	projectList: Projects[]
 }
 
 export function ProjectsDetails(props: Props) {
 	return (
-		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				transition={{ duration: 1.5 }}
-				className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-				{props.items.map((item) => (
-					<div key={item.title}>
-						<h2 className="text-3xl font-bold tracking-tight sm:text-4x">
-							Projetos
-						</h2>
+		<div className=" max-w-7xl px-4 sm:px-6 lg:px-8">
+			<div className="py-16 sm:py-24 xlg:max-w-none grid grid-cols-1 sm:grid-cols-3">
+				{props.projectList.map((project) => (
+					<Link
+						to={project.linkRep}
+						key={project.title}
+						className="lg:space-y-0 py-5 gap-x-8 gap-y-5 sm:gap-y-10 px-6 lg:px-8">
+						<motion.div
+							whileHover={{ scale: 1.2 }}
+							whileTap={{ scale: 0.8 }}
+							className="rounded p-px bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+							<div className="shadow-lg h-full w-full bg-primary-foreground px-6 py-4 ">
+								<div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+									<img
+										src={project.bannerImg}
+										alt="Imagem do projeto"
+										className="h-full w-full object-cover object-center"
+									/>
+								</div>
 
-						<div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0 py-10 gap-x-8 gap-y-20 px-6 lg:px-8 ">
-							<div className="group relative shadow-lg">
-								<motion.div
-									whileHover={{ scale: 1.2 }}
-									whileTap={{ scale: 0.8 }}
-									className="rounded p-px bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-									<div className="h-full w-full bg-primary-foreground px-6 py-4 ">
-										<div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-											<img
-												src={item.bannerImg}
-												alt="Imagem do projeto"
-												className="h-full w-full object-cover object-center"
-											/>
-										</div>
-										<h3 className="mt-6 text-sm flex items-center">
-											<Link to={"/home"}>
-												<span className="absolute inset-0" />
-											</Link>
-										</h3>
-										<p className="text-lg font-semibold">{item.title}</p>
-										<p className="mt-3 text-sm">{item.desc}</p>
-									</div>
-								</motion.div>
+								<p className="mt-6 flex items-center text-lg font-semibold">
+									{project.title}
+								</p>
+								<p className="mt-3 text-sm">{project.desc}</p>
 							</div>
-						</div>
-					</div>
+						</motion.div>
+					</Link>
 				))}
-			</motion.div>
+			</div>
 		</div>
 	)
 }
--- app/routes/_main.projects/route.tsx
+++ app/routes/_main.projects/route.tsx
@@ -1,19 +1,27 @@
 import { personalProject } from "~/assets/images"
 import { ProjectsDetails } from "./ProjectsDetails"
+import { motion } from "framer-motion"
 
 const projects = [
 	{
-		title: "Portfólio/Curriculo",
+		title: "Portfólio/Currículo",
 		desc: "Projeto com layout responsivo utilizando JavaScript, TypeScript, TailwindCSS, ReactJS e Remix",
 		bannerImg: personalProject,
-		socialIcons: [""],
+		linkRep: "https://github.com/pinuya/pinuya.dev",
 	},
 ]
 
 export default function Projects() {
 	return (
-		<div>
-			<ProjectsDetails items={projects} />
-		</div>
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 1.5 }}>
+			<h2 className="mt-20 text-3xl font-bold tracking-tight sm:text-4xl">
+				Projetos
+			</h2>
+
+			<ProjectsDetails projectList={projects} />
+		</motion.div>
 	)
 }
