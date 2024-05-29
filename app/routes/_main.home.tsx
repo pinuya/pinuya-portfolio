af5c53c51a74d3da31e08c1b4e995730070cacf0
Tifany <tifsnunes@gmail.com>
2024-05-29 17:01:05 -0300

    fix: footer height

--- app/routes/_main.home.tsx
+++ app/routes/_main.home.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 
 export default function Home() {
 	return (
-		<section className="flex flex-col items-center justify-center gap-y-10 flex-1 gap-x-28 sm:flex-row ">
+		<section className="flex flex-1 flex-col items-center justify-center gap-y-10 gap-x-28 h-[89vh] sm:flex-row">
 			<PageInitial />
 			<motion.img
 				initial={{ x: "100%" }}
--- app/routes/_main.tsx
+++ app/routes/_main.tsx
@@ -4,10 +4,10 @@ import Nav from "~/components/navbar/Nav"
 
 export default function Main() {
 	return (
-		<div className="p-6 flex flex-col h-screen w-full">
+		<div className="p-6 flex flex-col h-full w-full">
 			<Nav />
 
-			<div className="flex-1 flex items-center justify-center h-full">
+			<div className="flex-1 flex items-center justify-center ">
 				<Outlet />
 			</div>
 
