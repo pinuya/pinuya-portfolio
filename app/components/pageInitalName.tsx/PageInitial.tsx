2b2dcf6ed57a91aa6c0de58090c2e4b75693715f
Tifany <tifsnunes@gmail.com>
2024-08-14 20:53:46 -0300

    add: social media icons

--- app/components/pageInitalName.tsx/PageInitial.tsx
+++ app/components/pageInitalName.tsx/PageInitial.tsx
@@ -2,6 +2,12 @@ import { Link } from "@remix-run/react"
 import { Button } from "../ui/button"
 import { FileDown, MoveRight } from "lucide-react"
 import { motion } from "framer-motion"
+import {
+	GitHubLogoIcon,
+	InstagramLogoIcon,
+	LinkedInLogoIcon,
+	TwitterLogoIcon,
+} from "@radix-ui/react-icons"
 
 export default function PageInitial() {
 	const defaultAnimation = (duration: number) => {
@@ -16,39 +22,58 @@ export default function PageInitial() {
 	const buttonAnimation = defaultAnimation(1.5)
 
 	return (
-		<div>
-			<div className="flex flex-col items-center sm:items-start">
-				<motion.div {...titleAnimation}>
-					<h1 className="font-dancingScript text-4xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text ">
-						Tifany Alves
-					</h1>
-				</motion.div>
-
-				<motion.h2 {...roleAnimation} className="font-semibold ">
-					FullStack Developer | Software Engineer{" "}
-					<span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
-						.
-					</span>
-				</motion.h2>
-				<motion.p {...roleAnimation} className="text-sm mt-2 ">
-					Olá, seja bem vindo(a)! Prazer, sou{" "}
-					<span className="font-semibold">desenvolvedora FullStack</span>,{" "}
-					<br /> fique a vontade para conhecer melhor o meu trabalho.
-				</motion.p>
+		<div className="flex space-x-6">
+			<div className="flex flex-col space-y-6 justify-center">
+				<Link to={"https://github.com/pinuya"}>
+					<GitHubLogoIcon />
+				</Link>
+				<Link to={"https://x.com/tiffrieren"}>
+					<TwitterLogoIcon />
+				</Link>
+				<Link to={"https://www.instagram.com/tifsnunes/"}>
+					<InstagramLogoIcon />
+				</Link>
+				<Link to={"https://www.linkedin.com/in/tifanyanunes/"}>
+					<LinkedInLogoIcon />
+				</Link>
 			</div>
-			<motion.div {...buttonAnimation} className="mt-6 flex justify-between">
-				<Link to={"/about"}>
-					<Button className="">
-						{" "}
-						Saiba mais <MoveRight className="ml-2 h-4 w-4" />
-					</Button>
-				</Link>
-				<Link to={"/curriculoTifany.pdf"} target="_blank">
-					<Button variant="secondary">
-						Baixar Curriculo <FileDown className="ml-2 h-4 w-4" />
-					</Button>
-				</Link>
-			</motion.div>
+
+			<div>
+				<div className="flex flex-col items-center sm:items-start">
+					<motion.div {...titleAnimation}>
+						<h1 className="font-dancingScript text-6xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text ">
+							Tifany Alves
+						</h1>
+					</motion.div>
+
+					<motion.h2 {...roleAnimation} className="font-semibold ">
+						FullStack Developer | Software Engineer{" "}
+						<span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+							.
+						</span>
+					</motion.h2>
+					<motion.p {...roleAnimation} className="text-sm mt-2 ">
+						Olá, seja bem vindo(a)! Prazer, sou{" "}
+						<span className="font-semibold">desenvolvedora FullStack</span>,{" "}
+						<br /> Atualmente estou reformando meu portfolio, caso veja algo sem{" "}
+						<br />
+						animacao ou inacabado peco um pouco de paciencia rs.
+					</motion.p>
+				</div>
+				<motion.div {...buttonAnimation} className="mt-6 flex justify-between">
+					<Link to={"/about"}>
+						<Button className="">
+							{" "}
+							Saiba mais <MoveRight className="ml-2 h-4 w-4" />
+						</Button>
+					</Link>
+					<Link to={"/curriculoTifany.pdf"} target="_blank">
+						<Button variant="secondary">
+							Baixar Curriculo <FileDown className="ml-2 h-4 w-4" />
+						</Button>
+					</Link>
+				</motion.div>
+			</div>
 		</div>
 	)
 }
