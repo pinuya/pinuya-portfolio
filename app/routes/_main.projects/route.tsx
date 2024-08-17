b88b1cfe59f73e98fe1db0ac22ceb0382169d67f
Tifany <tifsnunes@gmail.com>
2024-08-17 12:26:39 -0300

    add: pagination on projects page

--- app/routes/_main.projects/route.tsx
+++ app/routes/_main.projects/route.tsx
@@ -4,35 +4,51 @@ import { motion } from "framer-motion"
 import {
 	Pagination,
 	PaginationContent,
-	PaginationEllipsis,
 	PaginationItem,
 	PaginationLink,
 	PaginationNext,
 	PaginationPrevious,
-} from "@/components/ui/pagination"
+} from "~/components/ui/pagination"
+import { type LoaderFunctionArgs, json } from "@remix-run/node"
+import { useLoaderData, useSearchParams } from "@remix-run/react"
 
-const projects = [
-	{
-		title: "Portfólio/Currículo",
-		desc: "Projeto com layout responsivo utilizando JavaScript, TypeScript, TailwindCSS, ReactJS e Remix",
-		bannerImg: personalProject,
-		linkRep: "https://github.com/pinuya/pinuya.dev",
-	},
-	{
-		title: "Dogs",
-		desc: "Projeto de estudos do curso Origamid. Com layout responsivo utilizando JavaScript, CSS e ReactJS.",
-		bannerImg: dogsProject,
-		linkRep: "https://github.com/pinuya/Dogs",
-	},
-	{
-		title: "Bikcraft",
-		desc: "Projeto de estudos do curso Origamid. Codigo refatorado com TypeScript e Remix.",
-		bannerImg: bikcraftProject,
-		linkRep: "https://github.com/pinuya/Bikecraft",
-	},
-]
+export const loader = (args: LoaderFunctionArgs) => {
+	const url = new URL(args.request.url)
+	const pageNumber = Number(url.searchParams.get("page") ?? 1)
+
+	const page = Number.isNaN(pageNumber) ? 1 : pageNumber
+	const quantity = 3
+
+	const projects = [
+		{
+			title: "Portfólio/Currículo",
+			desc: "Projeto com layout responsivo utilizando JavaScript, TypeScript, TailwindCSS, ReactJS e Remix",
+			bannerImg: personalProject,
+			linkRep: "https://github.com/pinuya/pinuya.dev",
+		},
+		{
+			title: "Dogs",
+			desc: "Projeto de estudos do curso Origamid. Com layout responsivo utilizando JavaScript, CSS e ReactJS.",
+			bannerImg: dogsProject,
+			linkRep: "https://github.com/pinuya/Dogs",
+		},
+		{
+			title: "Bikcraft",
+			desc: "Projeto de estudos do curso Origamid. Codigo refatorado com TypeScript e Remix.",
+			bannerImg: bikcraftProject,
+			linkRep: "https://github.com/pinuya/Bikecraft",
+		},
+	]
+
+	return json({
+		projects: projects.slice((page - 1) * quantity, page * quantity),
+		totalPage: Math.ceil(projects.length / quantity),
+	})
+}
 
 export default function Projects() {
+	const { projects, totalPage } = useLoaderData<typeof loader>()
+	const [searchParams, setSearchParams] = useSearchParams()
 	return (
 		<motion.div
 			className=""
@@ -43,22 +59,53 @@ export default function Projects() {
 
 			<ProjectsDetails projectList={projects} />
 
-			{/* <Pagination>
+			<Pagination>
 				<PaginationContent>
 					<PaginationItem>
-						<PaginationPrevious href="#" />
+						<PaginationPrevious
+							className="cursor-pointer"
+							onClick={() => {
+								const page = Number(searchParams.get("page") ?? 1)
+								if (page - 1 <= 0) {
+									return
+								}
+								setSearchParams((prev) => {
+									prev.set("page", (page - 1).toString())
+									return prev
+								})
+							}}
+						/>
 					</PaginationItem>
+					{Array.from({ length: totalPage }, (_value, key) => key).map((v) => (
+						<PaginationItem className="cursor-pointer" key={v}>
+							<PaginationLink
+								onClick={() => {
+									setSearchParams((prev) => {
+										prev.set("page", (v + 1).toString())
+										return prev
+									})
+								}}>
+								{v + 1}
+							</PaginationLink>
+						</PaginationItem>
+					))}
 					<PaginationItem>
-						<PaginationLink href="#">1</PaginationLink>
-					</PaginationItem>
-					<PaginationItem>
-						<PaginationEllipsis />
-					</PaginationItem>
-					<PaginationItem>
-						<PaginationNext href="#" />
+						<PaginationNext
+							className="cursor-pointer"
+							onClick={() => {
+								const page = Number(searchParams.get("page") ?? 1)
+								if (page + 1 > totalPage) {
+									return
+								}
+								setSearchParams((prev) => {
+									prev.set("page", (page + 1).toString())
+									return prev
+								})
+							}}
+						/>
 					</PaginationItem>
 				</PaginationContent>
-			</Pagination> */}
+			</Pagination>
 		</motion.div>
 	)
 }
