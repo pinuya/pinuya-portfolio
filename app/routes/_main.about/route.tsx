91a997ec44e1021fa9b05ea04bedd2ac4eaee00d
Tifany <tifsnunes@gmail.com>
2024-08-23 18:28:16 -0300

    add: new lang svg and new profile pic

--- app/routes/_main.about/route.tsx
+++ app/routes/_main.about/route.tsx
@@ -4,7 +4,11 @@ import { motion } from "framer-motion"
 import { Button } from "~/components/ui/button"
 import { FileDown } from "lucide-react"
 import { tifanyPfp } from "~/assets/images"
-import { IoShareSocialSharp } from "react-icons/io5"
+import { IoLogoJavascript, IoShareSocialSharp } from "react-icons/io5"
+import { SiJavascript, SiTypescript } from "react-icons/si"
+import { FaDocker, FaGitAlt, FaNodeJs, FaReact } from "react-icons/fa6"
+import { RiTailwindCssFill } from "react-icons/ri"
+import { BiLogoPostgresql } from "react-icons/bi"
 
 const educations = [
 	{
@@ -58,16 +62,16 @@ export default function About() {
 
 	return (
 		<div className="space-y-20">
-			<section>
+			<section className="space-y-4">
 				<motion.h1
 					{...titleAnimation}
 					className="text-3xl font-bold sm:text-4xl">
 					Sobre
 				</motion.h1>
-				<div className="flex flex-col gap-y-10 gap-x-28 sm:flex-row">
+				<div className="flex flex-col items-center gap-x-28 sm:flex-row">
 					<motion.p
 						{...descAnimation}
-						className="max-w-3xl tracking-tight px-6 py-10 gap-x-8 gap-y-20 lg:px-8 ">
+						className="max-w-3xl tracking-tight px-6 py-10 gap-x-8 lg:px-8 ">
 						Sou desenvolvedora FullStack, sempre busco aprimorar minhas
 						habilidades tanto em front-end quanto ao Back-end. Sou formada em{" "}
 						<span className="font-semibold">
@@ -90,7 +94,7 @@ export default function About() {
 						transition={{ duration: 1, ease: "easeOut" }}
 					/>
 				</div>
-				<div className="flex gap-x-8 gap-y-20 lg:px-8 ">
+				<div className="flex gap-x-8 lg:px-8 ">
 					<Link to={"/curriculoTifany.pdf"} target="_blank">
 						<Button variant="secondary">
 							Baixar Curriculo <FileDown className="ml-2 h-4 w-4" />
@@ -110,7 +114,16 @@ export default function About() {
 				<h2 className="text-2xl font-bold tracking-tight">
 					Linguagens e Tecnologias
 				</h2>
-				area em desenvolvimento...
+				<div className="flex flex-col md:flex-row space-x-6">
+					<SiJavascript className="size-12" />
+					<SiTypescript className="size-12" />
+					<FaNodeJs className="size-12" />
+					<FaReact className="size-12" />
+					<RiTailwindCssFill className="size-12" />
+					<FaDocker className="size-12" />
+					<BiLogoPostgresql className="size-12" />
+					<FaGitAlt className="size-12" />
+				</div>
 			</div>
 			<section>
 				<motion.h1
