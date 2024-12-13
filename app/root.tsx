e10d655120b32a9c22ea97f1240d3af2c26ce026
Tifany <tifsnunes@gmail.com>
2024-12-13 17:05:35 -0300

    fixes

--- app/root.tsx
+++ app/root.tsx
@@ -1,74 +1,78 @@
 import {
-	Links,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-	useLoaderData,
-} from "@remix-run/react"
-import "../tailwind.css"
-import clsx from "clsx"
-import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes"
-import { themeSessionResolver } from "./cookies"
-import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node"
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "@remix-run/react";
+import "./tailwind.css";
+import clsx from "clsx";
+import {
+  PreventFlashOnWrongTheme,
+  ThemeProvider,
+  useTheme,
+} from "remix-themes";
+import { themeSessionResolver } from "./cookies";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 
 export const links: LinksFunction = () => {
-	return [
-		{
-			href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
-			rel: "stylesheet",
-		},
-		{
-			rel: "preconnect",
-			href: "https://fonts.gstatic.com",
-			crossOrigin: "anonymous",
-		},
-		{
-			rel: "preconnect",
-			href: "https://fonts.googleapis.com",
-		},
-		{
-			rel: "stylesheet",
-			href: "https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap",
-		},
-	]
-}
+  return [
+    {
+      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+      rel: "stylesheet",
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "anonymous",
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap",
+    },
+  ];
+};
 
 // Return the theme from the session storage using the loader
 export async function loader({ request }: LoaderFunctionArgs) {
-	const { getTheme } = await themeSessionResolver(request)
-	return {
-		theme: getTheme(),
-	}
+  const { getTheme } = await themeSessionResolver(request);
+  return {
+    theme: getTheme(),
+  };
 }
 
 export default function AppWithProviders() {
-	const data = useLoaderData<typeof loader>()
-	return (
-		<ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
-			<App />
-		</ThemeProvider>
-	)
+  const data = useLoaderData<typeof loader>();
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  );
 }
 
 export function App() {
-	const data = useLoaderData<typeof loader>()
-	const [theme] = useTheme()
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
 
-	return (
-		<html lang="en" className={clsx(theme)}>
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
-				<Links />
-			</head>
-			<body className="font-poppins">
-				<Outlet />
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en" className={clsx(theme)}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
+        <Links />
+      </head>
+      <body className="font-poppins">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
