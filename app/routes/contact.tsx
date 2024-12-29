298757d614dfcaafd02576d56b2be4c3af843c58
Tifany <tifsnunes@gmail.com>
2024-12-29 12:47:24 -0300

    style: form

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -5,19 +5,26 @@
 
 // export default function Contact() {
 //   return (
-//     <div className="h-screen flex items-center justify-center flex-col gap-4">
-//       <h1 className="text-2xl text-foreground tracking-widest font-semibold">
+//     <div className="h-[84vh] flex items-center justify-center flex-col gap-6 px-4">
+//       <h1 className="text-4xl text-foreground tracking-wide font-bold text-center">
 //         <DecoderText text={"Diga olá"} delay={500} />
 //       </h1>
 
-//       <div className="gap-4 flex flex-col">
-//         <Input type="email" placeholder="Seu e-mail" />
-//         <Input type="text" placeholder="Mensagem" />
+//       <div className="shadow-md rounded-lg p-6 w-full max-w-md flex flex-col gap-4">
+//         <Input
+//           type="email"
+//           placeholder="Seu e-mail"
+//           className="p-3 border  rounded-md focus:outline-none focus:ring-1 focus:ring-purple-400"
+//         />
+//         <Input
+//           type="text"
+//           placeholder="Mensagem"
+//           className="p-3 border  rounded-md focus:outline-none focus:ring-2 focus:ring-purple-400"
+//         />
+//         <Button className="bg-purple-500 text-white py-3 rounded-md hover:bg-purple-600 hover:scale-105 transition-transform duration-200 flex items-center justify-center gap-2">
+//           <SendHorizontal /> Enviar
+//         </Button>
 //       </div>
-
-//       <Button className="bg-purple-400 gap-2">
-//         <SendHorizontal /> Envie sua mensagem
-//       </Button>
 //     </div>
 //   );
 // }
